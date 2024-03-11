836dcc43c96e997d5d6f6b21425c2c0ef87b74e7
宋利生 <1771081603@qq.com>
2024-03-11 16:20:26 +0800

    学习20240311

---
+++ 服务端/Java/chapter01/PersonInfo.java
@@ -0,0 +1,10 @@
+class PersonInfo{
+  public static void main(String[] args){
+//     System.out.println("姓名：宋利生");
+//     System.out.println("年龄：26");
+//     System.out.println("性别：男");
+    System.out.println("*    *");
+    System.out.println("\t*");
+    System.out.println("\n*");
+  }
+}
