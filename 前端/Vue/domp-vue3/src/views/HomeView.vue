490d79dae0615461be45c06bda1b37f9f9821402
宋利生 <1771081603@qq.com>
2024-06-26 23:28:32 +0800

    新建domp-vue3

---
+++ 前端/Vue/domp-vue3/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+const commonStore = useCounterStore()
+
+</script>
+
+<template>
+  <main>
+    <button @click="commonStore.increment">{{ commonStore.doubleCount }}</button>
+  </main>
+</template>
