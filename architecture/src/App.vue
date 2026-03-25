0ae7bb796584806343f6da403f8e7f8d389aec3d
宋利生 <e-songlisheng@chagee.com>
2026-03-25 10:24:54 +0800

    feat(App): 集成状态管理并添加计数器功能
    - 引入 counter store 状态管理
    - 使用 toRefs 处理响应式数据绑定
    - 在模板中显示计数值和双倍计数值
    - 添加点击按钮实现计数递增功能
    - 优化 tsconfig 配置文件格式

--- architecture/src/App.vue
+++ architecture/src/App.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+import { toRefs } from 'vue'
+
+const { count, doubleCount, increment } = toRefs(useCounterStore())
+</script>
 
 <template>
-    <h1>You did it!</h1>
-    <p>Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the documentation</p>
+    <h1>You did it!{{ count }}</h1>
+    <p>Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the documentation {{ doubleCount }}</p>
+    <button @click="increment">点击新增</button>
 </template>
 
 <style scoped></style>
