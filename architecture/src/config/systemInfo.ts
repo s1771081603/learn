2ca88d3dd3f8a81ee8efe8d3cf93f496f387bfde
宋利生 <e-songlisheng@chagee.com>
2026-04-03 17:38:34 +0800

    删除项目架构及相关文件
    - 移除前端架构文件，包括配置、组件、路由和状态管理
    - 删除代码规范、样式和依赖配置文件
    - 清理项目目录，移除所有相关文件和锁定文件

--- architecture/src/config/systemInfo.ts
+++
@@ -1,7 +0,0 @@
-import { ref } from "vue"
-
-export const SystemInfo = ref({
-    author: "宋利生",
-    email: "1771081603@qq.com",
-    telephone: 13477622913
-})
--- architecture/src/stores/index.ts
+++
@@ -1,9 +0,0 @@
-import { fullScreenStore } from "@/stores/fullScreen.ts"
-import { sidebarStore } from "@/stores/sidebar.ts"
-import { userInfoStore } from "@/stores/userInfo.ts"
-
-export const stores = {
-    fullScreenStore,
-    sidebarStore,
-    userInfoStore
-}
